62209b3bb2100aa44be33ef5d5634e3d0983f8de
shouyinji1 <shouyinji1@DESKTOP-HPB7UDD.lan>
2023-03-16 10:25:11 +0800

    update

--- Wacom/wacom_shortcuts.sh
+++ Wacom/wacom_shortcuts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 # Linux平台Wacom数位板快捷键触发脚本
+# 自由选择映射区域功能需要安装xdotool
 
 # Wacom CTL 472
 device="Wacom One by Wacom S Pen stylus"
@@ -35,7 +36,7 @@ getMonitorInfo(){
 	return 1	# 读取显示器参数失败
 }
 
-# 设置数位板感应区域
+# 获取数位板感应区域的适宜大小
 # 输入：屏幕大小size_x size_y
 # 输出：感应区域area_x area_y
 getDeviceArea(){
@@ -74,7 +75,7 @@ setDevice(){
 	baseAxisOfMonitor_x=${monitorInfo[2]}
 	baseAxisOfMonitor_y=${monitorInfo[3]}
 
-	if [ $1 -eq 1 ];then 	# 显示器0, 全屏映射
+	if [ "$1" == "-m0" ];then 	# 显示器0, 全屏映射
 		monitorID=0
 		monitorInfo=($(getMonitorInfo $monitorID))	# 获取显示器的分辨率
 		size_x=${monitorInfo[0]}
@@ -88,9 +89,9 @@ setDevice(){
 		xsetwacom set "$device" MapToOutput HEAD-${monitorID} \
 		&& xsetwacom set "$device" Rotate none \
 		&& xsetwacom set "$device" Area 0 0 $area_x $area_y \
-		&& notify-send "Ctrl+Alt+1: HEAD-${monitorID}屏幕，数位板不旋转, 全屏映射" \
+		&& notify-send "HEAD-${monitorID}屏幕，数位板不旋转, 全屏映射" \
 		&& echo $monitorID > $(dirname $(readlink -f "$0"))/Wacom-HEAD.tmp 	# 写入显示器选择文件
-	elif [ "$1" == "m1" ];then 	# 显示器1, 全屏映射
+	elif [ "$1" == "-m1" ];then 	# 显示器1, 全屏映射
 		monitorID=1
 		monitorInfo=($(getMonitorInfo $monitorID))	# 获取显示器的分辨率
 		size_x=${monitorInfo[0]}
@@ -102,34 +103,19 @@ setDevice(){
 		xsetwacom set "$device" MapToOutput HEAD-${monitorID} \
 		&& xsetwacom set "$device" Rotate none \
 		&& xsetwacom set "$device" Area 0 0 $area_x $area_y \
-		&& notify-send "Ctrl+Shift+1: HEAD-${monitorID}屏幕，数位板不旋转, 全屏映射" \
+		&& notify-send "HEAD-${monitorID}屏幕，数位板不旋转, 全屏映射" \
 		&& echo $monitorID > $(dirname $(readlink -f "$0"))/Wacom-HEAD.tmp 	# 写入显示器选择文件
-	elif [ $1 -eq 2 ];then 	# 左半屏映射
-		# 计算映射区域
-		size_x=$((size_x / 2))
-		deviceArea=($(getDeviceArea $size_y $size_x))
-		area_x=${deviceArea[0]}
-		area_y=${deviceArea[1]}
+	elif [ "$1" == "-a" ];then 	# 自由选定映射区域，以鼠标数位板指针为中心的1/2屏幕区域
+		eval $(xdotool getmouselocation --shell)	# 获取鼠标指针的X、Y、SCREEN、WINDOW变量值。需要安装xdotool工具
+		newSize_x=$((size_x/2))
+		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
+		basePoint_x=$((baseAxisOfMonitor_x + X - newSize_x/2))
+		basePoint_y=$((baseAxisOfMonitor_y + Y - newSize_y/2))
 
-		xsetwacom set "$device" Rotate ccw && \
-		xsetwacom set "$device" MapToOutput ${size_x}x${size_y}+${baseAxisOfMonitor_x}+${baseAxisOfMonitor_y} && \
-		xsetwacom set "$device" Area 0 0 $area_x $area_y && \
-		notify-send "Ctrl+Alt+3: 数位板顺时针旋转, 左半屏映射"
-	elif [ $1 -eq 3 ];then 	# 右半屏映射
-		# 计算显示器映射区域
-		size_x=$((size_x / 2))
-		basePoint_x=$((size_x+baseAxisOfMonitor_x))
-
-		# 计算数位板感应区
-		deviceArea=($(getDeviceArea $size_y $size_x))
-		area_x=${deviceArea[0]}
-		area_y=${deviceArea[1]}
-		#notify-send $area_x $area_y
-
-		xsetwacom set "$device" Rotate ccw && \
-		xsetwacom set "$device" MapToOutput ${size_x}x${size_y}+${basePoint_x}+${baseAxisOfMonitor_y} && \
-		xsetwacom set "$device" Area 0 0 $area_x $area_y && \
-		notify-send "Ctrl+Alt+2: 数位板顺时针旋转, 右半屏映射"
+		xsetwacom set "$device" Rotate none \
+		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${basePoint_y} \
+		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
+		&& notify-send "数位板不旋转, 映射区域以鼠标指针为中心的1/2宽度, Area全区域"
 	elif [ $1 -eq 4 ];then
 		newSize_x=$((size_x/2))
 		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
@@ -138,7 +124,7 @@ setDevice(){
 		xsetwacom set "$device" Rotate none \
 		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${baseAxisOfMonitor_x}+${basePoint_y} \
 		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+6: 数位板不旋转, 居左1/2宽度, Area全区域"
+		&& notify-send "数位板不旋转, 居左1/2宽度, Area全区域"
 	elif [ $1 -eq 5 ];then
 		newSize_x=$((size_x/2))
 		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
@@ -148,7 +134,7 @@ setDevice(){
 		xsetwacom set "$device" Rotate none \
 		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${basePoint_y} \
 		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+4: 数位板不旋转, 居中1/2宽度, Area全区域"
+		&& notify-send "数位板不旋转, 居中1/2宽度, Area全区域"
 	elif [ $1 -eq 6 ];then
 		newSize_x=$((size_x/2))
 		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
@@ -158,43 +144,69 @@ setDevice(){
 		xsetwacom set "$device" Rotate none \
 		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${basePoint_y} \
 		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+5: 数位板不旋转, 居右1/2宽度, Area全区域"
-	elif [ $1 -eq 7 ];then
-		newSize_x=$((size_x/2 + size_x * 2 / 100))	# 1/2宽度放大2%
-		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
+		&& notify-send "数位板不旋转, 居右1/2宽度, Area全区域"
+	#elif [ $1 -eq 2 ];then 	# 左半屏映射
+	#	# 计算映射区域
+	#	size_x=$((size_x / 2))
+	#	deviceArea=($(getDeviceArea $size_y $size_x))
+	#	area_x=${deviceArea[0]}
+	#	area_y=${deviceArea[1]}
 
-		xsetwacom set "$device" Rotate none \
-		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${baseAxisOfMonitor_x}+${baseAxisOfMonitor_y} \
-		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+7: 数位板不旋转, 左上略大于1/2宽度, Area全区域"
-	elif [ $1 -eq 8 ];then
-		newSize_x=$((size_x/2 + size_x * 2 / 100))
-		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
-		basePoint_y=$((size_y-newSize_y + baseAxisOfMonitor_y))
+	#	xsetwacom set "$device" Rotate ccw && \
+	#	xsetwacom set "$device" MapToOutput ${size_x}x${size_y}+${baseAxisOfMonitor_x}+${baseAxisOfMonitor_y} && \
+	#	xsetwacom set "$device" Area 0 0 $area_x $area_y && \
+	#	notify-send "Ctrl+Alt+3: 数位板顺时针旋转, 左半屏映射"
+	#elif [ $1 -eq 3 ];then 	# 右半屏映射
+	#	# 计算显示器映射区域
+	#	size_x=$((size_x / 2))
+	#	basePoint_x=$((size_x+baseAxisOfMonitor_x))
 
-		xsetwacom set "$device" Rotate none \
-		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${baseAxisOfMonitor_x}+${basePoint_y} \
-		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+8: 数位板不旋转, 左下略大于1/2宽度, Area全区域"
-	elif [ $1 -eq 9 ];then
-		newSize_x=$((size_x/2 + size_x * 2 / 100))
-		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
-		basePoint_x=$((size_x-newSize_x + baseAxisOfMonitor_x))
+	#	# 计算数位板感应区
+	#	deviceArea=($(getDeviceArea $size_y $size_x))
+	#	area_x=${deviceArea[0]}
+	#	area_y=${deviceArea[1]}
+	#	#notify-send $area_x $area_y
 
-		xsetwacom set "$device" Rotate none \
-		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${baseAxisOfMonitor_y} \
-		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+9: 数位板不旋转, 右上略大于1/2宽度, Area全区域"
-	elif [ $1 -eq 0 ];then
-		newSize_x=$((size_x/2 + size_x * 2 / 100))
-		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
-		basePoint_x=$((size_x-newSize_x + baseAxisOfMonitor_x))
-		basePoint_y=$((size_y-newSize_y + baseAxisOfMonitor_y))
+	#	xsetwacom set "$device" Rotate ccw && \
+	#	xsetwacom set "$device" MapToOutput ${size_x}x${size_y}+${basePoint_x}+${baseAxisOfMonitor_y} && \
+	#	xsetwacom set "$device" Area 0 0 $area_x $area_y && \
+	#	notify-send "Ctrl+Alt+2: 数位板顺时针旋转, 右半屏映射"
+	#elif [ $1 -eq 7 ];then
+	#	newSize_x=$((size_x/2 + size_x * 2 / 100))	# 1/2宽度放大2%
+	#	newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
 
-		xsetwacom set "$device" Rotate none \
-		&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${basePoint_y} \
-		&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
-		&& notify-send "Ctrl+Alt+0: 数位板不旋转, 右下略大于1/2宽度, Area全区域"
+	#	xsetwacom set "$device" Rotate none \
+	#	&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${baseAxisOfMonitor_x}+${baseAxisOfMonitor_y} \
+	#	&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
+	#	&& notify-send "Ctrl+Alt+7: 数位板不旋转, 左上略大于1/2宽度, Area全区域"
+	#elif [ $1 -eq 8 ];then
+	#	newSize_x=$((size_x/2 + size_x * 2 / 100))
+	#	newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
+	#	basePoint_y=$((size_y-newSize_y + baseAxisOfMonitor_y))
+
+	#	xsetwacom set "$device" Rotate none \
+	#	&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${baseAxisOfMonitor_x}+${basePoint_y} \
+	#	&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
+	#	&& notify-send "Ctrl+Alt+8: 数位板不旋转, 左下略大于1/2宽度, Area全区域"
+	#elif [ $1 -eq 9 ];then
+	#	newSize_x=$((size_x/2 + size_x * 2 / 100))
+	#	newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
+	#	basePoint_x=$((size_x-newSize_x + baseAxisOfMonitor_x))
+
+	#	xsetwacom set "$device" Rotate none \
+	#	&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${baseAxisOfMonitor_y} \
+	#	&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
+	#	&& notify-send "Ctrl+Alt+9: 数位板不旋转, 右上略大于1/2宽度, Area全区域"
+	#elif [ $1 -eq 0 ];then
+	#	newSize_x=$((size_x/2 + size_x * 2 / 100))
+	#	newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))
+	#	basePoint_x=$((size_x-newSize_x + baseAxisOfMonitor_x))
+	#	basePoint_y=$((size_y-newSize_y + baseAxisOfMonitor_y))
+
+	#	xsetwacom set "$device" Rotate none \
+	#	&& xsetwacom set "$device" MapToOutput ${newSize_x}x${newSize_y}+${basePoint_x}+${basePoint_y} \
+	#	&& xsetwacom set "$device" Area 0 0 ${defaultArea[2]} ${defaultArea[3]} \
+	#	&& notify-send "Ctrl+Alt+0: 数位板不旋转, 右下略大于1/2宽度, Area全区域"
 	fi
 }
 
