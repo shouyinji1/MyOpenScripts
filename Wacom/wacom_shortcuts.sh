127aececae57743203fca6e2951643fdedb199c2
shouyinji1 <you@example.com>
2023-07-13 20:20:06 +0800

    update

--- Wacom/wacom_shortcuts.sh
+++ Wacom/wacom_shortcuts.sh
@@ -4,8 +4,13 @@
 # 自由选择映射区域功能需要安装xdotool
 
 # Wacom CTL 472
-device="Wacom One by Wacom S Pen stylus"
-defaultArea=(0 0 15200 9500)
+#device="Wacom One by Wacom S Pen stylus"
+#defaultArea=(0 0 15200 9500)
+
+# Wacom CTL 672
+device="Wacom One by Wacom M Pen stylus"
+defaultArea=(0 0 21600 13500)
+
 
 # 获取显示器分辨率和基轴坐标
 # 输入：显示器编号0,1,...
@@ -111,8 +116,11 @@ setDevice(){
 		fi
 	elif [ "$1" == "-a" ];then 	# 自由选定映射区域，1/2屏幕宽度，指针位于映射中心的左上方
 		eval $(xdotool getmouselocation --shell)	# 获取鼠标指针的X、Y、SCREEN、WINDOW变量值。需要安装xdotool工具
-		#newSize_x=$((size_x/2 * 100/110))	# 缩放至90%大小，适用15.6寸屏
-		newSize_x=$((size_x/2))	# 1/2屏幕宽度
+		if [ "$device" == "Wacom One by Wacom M Pen stylus" ];then
+			newSize_x=$((size_x/2 * 100/80))	# 缩放至120%大小，适用15.6寸屏幕，Wacom CTL 672
+		else
+			newSize_x=$((size_x/2))	# 1/2屏幕宽度
+		fi
 		newSize_y=$((newSize_x * defaultArea[3] / defaultArea[2]))	# 保持长宽比
 		basePoint_x=$((X - newSize_x/3))	# 鼠标指针位于映射中心的左上方
 		basePoint_y=$((Y - newSize_y/4))
