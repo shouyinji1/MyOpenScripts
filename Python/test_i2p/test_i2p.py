56d8df2c2e459d9b5f21880e190edc7ce22dc6b3
shouyinji1 <shouyinji1@debian>
2024-01-14 11:41:35 +0800

    update

---
+++ Python/test_i2p/test_i2p.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def test_i2p():
+    proxies = {
+        "http": 'socks5://127.0.0.1:9050',
+        'https': 'socks5://127.0.0.1:9050'
+    }
+    try:
+        requests.get('http://www.baidu.com', proxies=proxies)
+    except ConnectionError:
+        return False
+    return True
+
+
+if __name__=='__main__':
+    if test_i2p() == False:
+        if test_i2p() == False:
+            if test_i2p() == False:
+                os.system("systemctl restart i2p")
